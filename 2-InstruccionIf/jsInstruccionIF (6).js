2b844d9367277100ad96f370681af0d0b87480b5
Briandemon <40809452+Briandemon@users.noreply.github.com>
2018-07-10 22:16:14 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad
 
+edad = parseInt(document.getElementById("edad").value);
 
+if (edad >17){
+
+    alert("Es adulto")
+}
+else if(edad >=13 && edad <= 17 )
+{
+
+alert ("Es adolecente")
+}
+
+else
+{
+alert("Es menor")
+}
 
 }//FIN DE LA FUNCIÓN
